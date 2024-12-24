7cb72cd0b2d6afe8204441a8d9aa4dce8cc31671
Royal <kingstars@gmail.com>
2024-12-24 14:21:56 -0500

    optimize the c code in 28. Find the Index of the First Occurrence in a String

--- 28. Find the Index of the First Occurrence in a String/main.c
+++ 28. Find the Index of the First Occurrence in a String/main.c
@@ -15,7 +15,7 @@ int strStr(char *haystack, char *needle) {
     int len1 = strlen(haystack);
     int len2 = strlen(needle);
 
-    for (int i = 0; i < len1; i++) {
+    for (int i = 0; i <= len1 - len2; i++) {
         int j = 0;
         while (j < len2 && haystack[i + j] == needle[j]) {
             j++;
