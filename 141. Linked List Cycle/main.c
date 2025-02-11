4ab161eed3d1290342bba4c9da28fc835f4c8653
Royal <kingstars@gmail.com>
2025-02-11 16:19:12 -0500

    add solutions for leetcode problem 141. Linked List Cycle

--- 141. Linked List Cycle/main.c
+++ 141. Linked List Cycle/main.c
@@ -1,6 +1,6 @@
+#include <limits.h>
 #include <stdbool.h>
 #include <stdio.h>
-#include <limits.h>
 
 struct ListNode {
     int val;
@@ -47,5 +47,38 @@ bool hasCycle(struct ListNode *head) {
         head = head->next;
     }
 
+    return false;
+}
+
+#define ARRAY_SIZE 10000
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+bool hasCycle(struct ListNode *head) {
+    struct ListNode **array = (struct ListNode **)calloc(ARRAY_SIZE, sizeof(struct ListNode *));
+    if (array == NULL) {
+        return false;
+    }
+
+    while (head != NULL) {
+        size_t i = 0;
+        while (array[i] != NULL) {
+            if (head == array[i]) {
+                free(array);
+                return true;
+            }
+            i++;
+        }
+
+        array[i] = head;
+        head = head->next;
+    }
+
+    free(array);
     return false;
 }
