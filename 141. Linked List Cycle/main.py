1be35b386cacb8c0c818cbf0ab3c6643d1050729
Royal <kingstars@gmail.com>
2025-02-11 21:59:49 -0500

    add solutions for leetcode problem 141. Linked List Cycle

--- 141. Linked List Cycle/main.py
+++ 141. Linked List Cycle/main.py
@@ -8,6 +8,7 @@ class ListNode:
         self.val = x
         self.next = None
 
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, x):
@@ -22,6 +23,7 @@ class Solution:
             head = head.next
 
         return False
+
 
 # Definition for singly-linked list.
 # class ListNode:
@@ -68,6 +70,24 @@ class Solution:
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         nodes = dict()
+
+        while head is not None:
+            if head in nodes:
+                return True
+            nodes[head] = 1
+            head = head.next
+
+        return False
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        nodes = defaultdict(int)
 
         while head is not None:
             if head in nodes:
