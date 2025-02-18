50e84de066709cce7fbaa085ceb88bd52f7150eb
Royal <kingstars@gmail.com>
2025-02-17 21:29:35 -0500

    add solutions for leetcode problem 121. Best Time to Buy and Sell Stock

--- 121. Best Time to Buy and Sell Stock/main.rs
+++ 121. Best Time to Buy and Sell Stock/main.rs
@@ -1,3 +1,5 @@
+struct Solution;
+
 impl Solution {
     pub fn max_profit(prices: Vec<i32>) -> i32 {
         let mut max_prof = 0;
@@ -13,5 +15,18 @@ impl Solution {
         }
 
         max_prof
+    }
+}
+
+use std::cmp::{max, min};
+
+impl Solution {
+    pub fn max_profit(prices: Vec<i32>) -> i32 {
+        prices
+            .iter()
+            .fold((0, i32::MAX), |(profit, buy), &price| {
+                (max(profit, price - buy), min(buy, price))
+            })
+            .0
     }
 }
