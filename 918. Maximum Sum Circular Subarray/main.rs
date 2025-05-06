656e4cdfcd5dc9a4e95ce9b3d14cadfcfcbd76d3
Royal <kingstars@gmail.com>
2025-05-06 00:02:16 -0400

    add solutions for leetcode problem 918. Maximum Sum Circular Subarray

--- 918. Maximum Sum Circular Subarray/main.rs
+++ 918. Maximum Sum Circular Subarray/main.rs
@@ -2,6 +2,24 @@ struct Solution;
 
 impl Solution {
     pub fn max_subarray_sum_circular(nums: Vec<i32>) -> i32 {
-        let max_sum = i32::MIN;
+        let mut max_sum = i32::MIN;
+        let mut min_sum = i32::MAX;
+        let mut cur_max = 0;
+        let mut cur_min = 0;
+        let mut total_sum = 0;
+
+        for num in nums {
+            cur_max = num.max(cur_max + num);
+            max_sum = max_sum.max(cur_max);
+            cur_min = num.min(cur_min + num);
+            min_sum = min_sum.min(cur_min);
+            total_sum += num;
+        }
+
+        if max_sum < 0 {
+            return max_sum;
+        }
+
+        max_sum.max(total_sum - min_sum)
     }
 }
