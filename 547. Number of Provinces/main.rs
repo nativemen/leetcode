1ba2cdfa608939ff2a610be82ed64e34218fe8ed
Royal <kingstars@gmail.com>
2025-06-30 22:23:47 -0400

    add solutions for leetcode problem 547. Number of Provinces

--- 547. Number of Provinces/main.rs
+++ 547. Number of Provinces/main.rs
@@ -26,9 +26,43 @@ impl Solution {
     }
 }
 
-
 impl Solution {
     pub fn find_circle_num(is_connected: Vec<Vec<i32>>) -> i32 {
+        fn find(parent: &mut Vec<usize>, x: usize) -> usize {
+            if parent[x] != x {
+                parent[x] = find(parent, parent[x]);
+            }
 
+            parent[x]
+        }
+
+        fn union_set(parent: &mut Vec<usize>, x: usize, y: usize) {
+            let root_x = find(parent, x);
+            let root_y = find(parent, y);
+
+            if root_x != root_y {
+                parent[root_x] = root_y;
+            }
+        }
+
+        let mut parent = vec![0; is_connected.len()];
+
+        parent.iter_mut().enumerate().for_each(|(i, val)| {
+            *val = i;
+        });
+
+        for i in 0..is_connected.len() {
+            for j in 0..is_connected[i].len() {
+                if is_connected[i][j] == 1 {
+                    union_set(&mut parent, i, j);
+                }
+            }
+        }
+
+        parent
+            .iter()
+            .enumerate()
+            .filter(|&(i, &val)| val == i)
+            .count() as i32
     }
 }
